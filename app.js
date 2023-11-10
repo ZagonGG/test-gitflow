6bb3405e6fe12b36b4a6ff009d6cdea953524dd9
Gonzalo Zarate <zagon3d@gmail.com>
2023-11-10 15:17:00 -0300

    Se limpio el codigo

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ console.log("object");
 function sayHello () {
     console.log("Hola, Como estan?");
 }
-<<<<<<< HEAD
-=======
+
+
 function sayBye (){
     console.log("Adios!");
 }
->>>>>>> feature/say-bye
